d9b17fc9c755f3f5138ade3f89d64c522e3861d1
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-11-14 16:28:57 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const { ActivityType } = require('discord.js');
+
+module.exports = {
+  ownerId: '1004206704994566164',
+  status: {
+    rotateDefault: [
+      { name: 'Watching Servers', type: ActivityType.Watching },
+      { name: 'Fun', type: ActivityType.Playing },
+      { name: 'on YouTube', type: ActivityType.Streaming,
+      { name: 'Spotify', type: ActivityType.Custom },
+    ],
+    songStatus: true
+  },
+  spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
+}
+
---
+++ lavalink.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    enabled: true, 
+    lavalink: {
+      name: "owbzgr1k",
+      password: "G9ehOVbom7f",
+      host: "23.80.88.110",
+      port:  10152,
+      secure: false
+    }
+};
+
+
